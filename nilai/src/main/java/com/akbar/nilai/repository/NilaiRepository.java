f2e076217e58652107a8d05bb1f3c180b253cc1b
AkbarRamadhan18 <azzammilakbarramadhan@gmail.com>
2023-12-14 22:30:11 +0700

    Spring-Boot

---
+++ nilai/src/main/java/com/akbar/nilai/repository/NilaiRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.akbar.nilai.repository;
+
+import com.akbar.nilai.entity.Nilai;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+/**
+ *
+ * @author Dell
+ */
+public interface NilaiRepository extends JpaRepository<Nilai, Long>{
+    
+}
